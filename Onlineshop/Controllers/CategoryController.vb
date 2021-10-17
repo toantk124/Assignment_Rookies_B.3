bf8a97c6a711d60b8be4a8c6dea9897723ee8899
unknown <you@example.com>
2021-10-17 17:49:31 +0700

    first commit

---
+++ Onlineshop/Controllers/CategoryController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class CategoryController
+        Inherits Controller
+
+        ' GET: Category
+        Function Category() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
---
+++ Onlineshop/Controllers/ProductController.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class ProductController
+        Inherits Controller
+
+        ' GET: Product
+        Function Details() As ActionResult
+            Return View()
+        End Function
+
+        Function ProductRating() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
